3c94f6164e3e4376bcaf322572d307b35d04b303
Tanmay211 <tanmaygupta211@gmail.com>
2019-12-21 11:52:13 +0530

    Adjusting the package versions for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;  // basic setup
 
-mongoose.connect(process.env.MONGODB_URI ,{useNewUrlParser: true}); // defining the connection to the database running at localhost
+mongoose.connect(process.env.MONGODB_URI); /*,{useNewUrlParser: true}); required in the latest version */
+ // defining the connection to the database running at localhost or heroku
 
 module.exports = { mongoose };
