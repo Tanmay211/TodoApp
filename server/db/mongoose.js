4530eef08d57718b4f606a1d77f2699874acdf0b
Tanmay211 <tanmaygupta211@gmail.com>
2020-05-12 20:20:58 +0530

    connection issue resolved

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;  // basic setup
 
-mongoose.connect("mongodb+srv://tanmay:githubcena123@cluster0-oidqo.gcp.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true}); // required in the latest version */
+const uri = `mongodb+srv://admin:root@cluster0-c3skp.mongodb.net/test?retryWrites=true&w=majority`
+
+mongoose.connect(`${uri}`, {useNewUrlParser: true})
+.then((res) => {
+    console.log(res);
+}, (err) => {
+    console.log("Error is ",err);
+});
+ // required in the latest version */
  // defining the connection to the database running at localhost or heroku
 
 module.exports = { mongoose };
